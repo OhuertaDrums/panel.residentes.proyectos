8b630d0d9e99fd2010cab1e468cc4cb75b5561dd
Ohuerta <ohuerta@yezdo.com>
2020-01-16 17:07:56 -0600

    cambios de agendar

--- banco_proyectos/urls.py
+++ banco_proyectos/urls.py
@@ -5,12 +5,10 @@ from . import views
 app_name = 'banco_proyectos	'
 urlpatterns = [
 
-    path('', views.index, name='index'),
-    path('base', views.base, name='base'),
-    path('prueba', views.pruebas, name='pruebas'),
-    path('registrar', views.registrar, name='registrar'),
     path('login', views.login, name='login'),
-    path('agendarcita', views.agendarcita, name='agendarcita'),
+    path('vistaprincipal', views.vistaprincipal, name='vistaprincipal'), 
+    path('registrar', views.registrar, name='registrar'),
+    path('agendar', views.agendar, name='agendar'),
     path('residentes', views.residentes, name='residentes'),
     path('archivosresidente', views.archivosresidente, name= 'archivosresidente'),
     path('restablecer-contraseña', views.restablecercontraseña, name= 'restablecer'),
